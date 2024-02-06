a72da4809a8efd8c319f9ece3b8844fb8d82fd48
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-02-06 15:32:31 -0500

    2.6 Generating conditional output.

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -1,12 +1,15 @@
 import JavaScript from "./JavaScript";
 import Classes from "./Classes";
 import PathParameters from "./routing/PathParameters";
-
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
 
 function Assignment3() {
     return(
         <div className="container">
             <h2>Assignment 3</h2>
+            <ConditionalOutput/>
+            <Styles/>
             <Classes/>
             <PathParameters/>
             <JavaScript/>
