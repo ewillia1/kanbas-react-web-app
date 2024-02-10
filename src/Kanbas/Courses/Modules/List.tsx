0d0f3aa5fc68386b8769c2fab171107fff47f1fa
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-02-10 11:14:04 -0500

    Cleaned of Modules List code.

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -9,54 +9,6 @@ function ModuleList() {
     const { courseId } = useParams();
     const modulesList = modules.filter((module) => module.course === courseId);
     const [selectedModule, setSelectedModule] = useState(modulesList[0]);
-    
-    // const [icon, setIcon] = useState(false);
-    // useEffect(() => {
-        // console.log("icon = " + icon);
-        // if (icon) {
-        //     setIcon(false);
-        // } else {
-        //     setIcon(true);
-        // }
-
-        // let _iconID = selectedModule.id_icon;
-        // if (typeof(_iconID) === 'string') {
-        //     let x = document.getElementById(_iconID);
-        //     if (x != null) {
-        //         x.classList.toggle("fa-caret-down");
-        //     }
-        // }
-        
-    // }, [selectedModule]);
-
-    // function changeModuleIcon(module: React.SetStateAction<{ _id: string; name: string; description: string; course: string; id_icon: string; lessons: { _id: string; name: string; description: string; module: string; }[]; } | { _id: string; name: string; description: string; course: string; id_icon?: undefined; lessons?: undefined; } | { _id: string; name: string; description: string; course: string; lessons: { _id: string; name: string; description: string; module: string; }[]; id_icon?: undefined; }>, _iconID: string | undefined) {
-    //     setSelectedModule(module);
-
-    //     if (typeof(_iconID) === 'string') {
-    //         let x = document.getElementById(_iconID);
-    //         if (x !== null) {
-    //             x.classList.toggle("fa-caret-down");
-    //         }
-    //     }
-    // }
-
-    function RightCaret() {
-        return <FaCaretRight style={{paddingRight: "5px"}}/>;
-    }
-      
-    function DownCaret() {
-        return <FaCaretDown style={{paddingRight: "5px"}}/>;
-    }
-
-    // function ChangeModuleIcon(conditionSt: boolean) {
-    //     if (conditionSt) {
-    //         return <DownCaret/>;
-    //     } else {
-    //         return <RightCaret/>;
-    //     }
-    // }
-
-    // let x = false;
 
     return (
         <>
@@ -78,23 +30,10 @@ function ModuleList() {
 
             <ul className="list-group wd-modules">
                 {modulesList.map((module) => (
-                    // <li key={module._id} className="list-group-item" onClick={() => changeModuleIcon(module, module.id_icon)}>
                     <li key={module._id} className="list-group-item" onClick={() => setSelectedModule(module)}>
                         <div>
                             <RxDragHandleDots2 className="me-2" />
-
-                            {/* { icon ? <FaCaretRight style={{paddingRight: "5px"}} id={module.icon_id} /> : <FaCaretDown style={{paddingRight: "5px"}} id={module.icon_id} /> } {module.name} */}
-                            
-                            {/* <i id={module.id_icon} className={(selectedModule._id === module._id ? "fa-solid fa-caret-down" : "fa-solid fa-caret-right")}</i> */}
-
-                            {/* <i id={module.id_icon} className={classNames('fa-solid', selectedModule._id === module._id ? "fa-caret-down" : "fa-caret-right")}</i> */}
-
-                            {/* {selectedModule._id === module._id && x = {true} : x = {false}
-                            } */}
-
-                            {/* {selectedModule._id === module._id && <DownCaret/> : <RightCaret/>} */}
-
-                            {selectedModule._id === module._id ? <FaCaretDown style={{paddingRight: "5px"}}/> : <FaCaretRight style={{paddingRight: "5px"}}/>}
+                            {selectedModule._id === module._id ? <FaCaretDown style={{paddingRight: "5px"}}/> : <FaCaretRight style={{paddingRight: "5px"}}/>} {module.name}
                             
                             <span className="float-end">
                                 <FaCheckCircle className="text-success" />
