f7636927e76dfe6903c4977dbc24edacd89a8a28
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-02-10 11:10:54 -0500

    Got caret icons to move as expected in Modules List.

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./index.css";
 import { modules } from "../../Database";
-import { FaEllipsisV, FaCheckCircle, FaPlusCircle, FaRegCheckCircle, FaPlus, FaCaretDown, FaAngleRight } from "react-icons/fa";
+import { FaEllipsisV, FaCheckCircle, FaPlusCircle, FaRegCheckCircle, FaPlus, FaCaretDown, FaAngleRight, FaCaretRight } from "react-icons/fa";
 import { useParams } from "react-router";
 import { RxDragHandleDots2 } from "react-icons/rx";
 
@@ -11,26 +11,52 @@ function ModuleList() {
     const [selectedModule, setSelectedModule] = useState(modulesList[0]);
     
     // const [icon, setIcon] = useState(false);
-
     // useEffect(() => {
-    //     console.log("icon = " + icon);
-    //     if (icon) {
-    //         setIcon(false);
-    //     } else {
-    //         setIcon(true);
-    //     }
+        // console.log("icon = " + icon);
+        // if (icon) {
+        //     setIcon(false);
+        // } else {
+        //     setIcon(true);
+        // }
+
+        // let _iconID = selectedModule.id_icon;
+        // if (typeof(_iconID) === 'string') {
+        //     let x = document.getElementById(_iconID);
+        //     if (x != null) {
+        //         x.classList.toggle("fa-caret-down");
+        //     }
+        // }
+        
     // }, [selectedModule]);
 
-    function changeModuleIcon(module: React.SetStateAction<{ _id: string; name: string; description: string; course: string; id_icon: string; lessons: { _id: string; name: string; description: string; module: string; }[]; } | { _id: string; name: string; description: string; course: string; id_icon?: undefined; lessons?: undefined; } | { _id: string; name: string; description: string; course: string; lessons: { _id: string; name: string; description: string; module: string; }[]; id_icon?: undefined; }>, _iconID: string | undefined) {
-        setSelectedModule(module);
+    // function changeModuleIcon(module: React.SetStateAction<{ _id: string; name: string; description: string; course: string; id_icon: string; lessons: { _id: string; name: string; description: string; module: string; }[]; } | { _id: string; name: string; description: string; course: string; id_icon?: undefined; lessons?: undefined; } | { _id: string; name: string; description: string; course: string; lessons: { _id: string; name: string; description: string; module: string; }[]; id_icon?: undefined; }>, _iconID: string | undefined) {
+    //     setSelectedModule(module);
 
-        if (typeof(_iconID) === 'string') {
-            let x = document.getElementById(_iconID);
-            if (x !== null) {
-                x.classList.toggle("fa-caret-down");
-            }
-        }
+    //     if (typeof(_iconID) === 'string') {
+    //         let x = document.getElementById(_iconID);
+    //         if (x !== null) {
+    //             x.classList.toggle("fa-caret-down");
+    //         }
+    //     }
+    // }
+
+    function RightCaret() {
+        return <FaCaretRight style={{paddingRight: "5px"}}/>;
     }
+      
+    function DownCaret() {
+        return <FaCaretDown style={{paddingRight: "5px"}}/>;
+    }
+
+    // function ChangeModuleIcon(conditionSt: boolean) {
+    //     if (conditionSt) {
+    //         return <DownCaret/>;
+    //     } else {
+    //         return <RightCaret/>;
+    //     }
+    // }
+
+    // let x = false;
 
     return (
         <>
@@ -52,12 +78,23 @@ function ModuleList() {
 
             <ul className="list-group wd-modules">
                 {modulesList.map((module) => (
-                    <li key={module._id} className="list-group-item" onClick={() => changeModuleIcon(module, module.id_icon)}>
+                    // <li key={module._id} className="list-group-item" onClick={() => changeModuleIcon(module, module.id_icon)}>
+                    <li key={module._id} className="list-group-item" onClick={() => setSelectedModule(module)}>
                         <div>
                             <RxDragHandleDots2 className="me-2" />
+
                             {/* { icon ? <FaCaretRight style={{paddingRight: "5px"}} id={module.icon_id} /> : <FaCaretDown style={{paddingRight: "5px"}} id={module.icon_id} /> } {module.name} */}
                             
-                            <i id={module.id_icon} className="fa-solid fa-caret-right"></i>
+                            {/* <i id={module.id_icon} className={(selectedModule._id === module._id ? "fa-solid fa-caret-down" : "fa-solid fa-caret-right")}</i> */}
+
+                            {/* <i id={module.id_icon} className={classNames('fa-solid', selectedModule._id === module._id ? "fa-caret-down" : "fa-caret-right")}</i> */}
+
+                            {/* {selectedModule._id === module._id && x = {true} : x = {false}
+                            } */}
+
+                            {/* {selectedModule._id === module._id && <DownCaret/> : <RightCaret/>} */}
+
+                            {selectedModule._id === module._id ? <FaCaretDown style={{paddingRight: "5px"}}/> : <FaCaretRight style={{paddingRight: "5px"}}/>}
                             
                             <span className="float-end">
                                 <FaCheckCircle className="text-success" />
