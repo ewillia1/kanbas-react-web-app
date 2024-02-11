446469a2332bdbd177abb4c86203b98cb18dfaa8
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-02-11 15:44:25 -0500

    Updated Course Home layout.

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -7,7 +7,7 @@ function Home() {
             <div className="col-sm-12 col-md-12 col-lg-12 col-xl-9">
                 <ModuleList />
             </div>
-            <div className="col-xl-3 d-none d-xl-block">
+            <div className="col-xl-3 d-none d-xl-block" style={{paddingLeft: "revert-layer"}}>
                 <Status/>
             </div>
         </div>
