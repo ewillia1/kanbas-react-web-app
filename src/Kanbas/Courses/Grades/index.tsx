a401fb1e07cad00b41bb0ce94813fa00f0bb3fa6
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-02-12 20:12:29 -0500

    Working on Grades code. Updated Nav.tsx.

---
+++ src/Kanbas/Courses/Grades/index.tsx
@@ -0,0 +1,94 @@
+import { assignments, enrollments, grades, users } from "../../Database";
+import { Link, useParams } from "react-router-dom";
+import "./index.css";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { SlArrowDown } from "react-icons/sl";
+import { CiFilter } from "react-icons/ci";
+
+function Grades() {
+    const { courseId } = useParams();
+    const as = assignments.filter((assignment) => assignment.course === courseId);
+    const es = enrollments.filter((enrollment) => enrollment.course === courseId);
+
+    function myChangeFunction() {
+        // TODO document why this function 'myChangeFunction' is empty
+    }
+
+    return (
+        <div>
+            <button type="button" className="wd-float-right wd-rounded-corners-all-around wd-home-button wd-margin-5px"><i className="fa-solid fa-gear"></i></button>
+            <button type="button" className="wd-float-right wd-rounded-corners-all-around wd-home-button wd-margin-5px"><i className="fa-solid fa-file-export"></i> Export</button>
+            <button type="button" className="wd-float-right wd-rounded-corners-all-around wd-home-button wd-margin-5px"><i className="fa-solid fa-file-import"></i> Import</button>
+            <div className="wd-float-done"></div>
+
+            <div className="row">
+                <div className="col-sm">
+                    <label htmlFor="studentList" className="form-label">Student Name</label>
+                    <div className="input-group">
+                        <div className="input-group-text"><FaMagnifyingGlass /></div>
+                        <input type="text" className="form-control wd-search-dropdown" id="studentList" list="studentListOptions" placeholder="Search Students" onChange={myChangeFunction}/>
+                        <div className="input-group-text wd-dropdown-angle"><SlArrowDown /></div>
+                    </div>
+                    <datalist id="studentListOptions">
+                        {es.map((enrollment) => {
+                            const user = users.find((user) => user._id === enrollment.user);
+                            return(
+                                <option>{user?.firstName} {user?.lastName}</option>
+                            );
+                        })}
+                    </datalist>
+                </div>
+                <div className="col-sm">
+                    <label htmlFor="assignmentList" className="form-label">Assignment Names</label>
+                    <div className="input-group">
+                        <div className="input-group-text"><FaMagnifyingGlass /></div>
+                        <input type="text" className="form-control wd-search-dropdown" id="assignmentList" list="assignmentListOptions" placeholder="Search Assignments" onChange={myChangeFunction}/>
+                        <div className="input-group-text wd-dropdown-angle"><SlArrowDown /></div>
+                    </div>
+                    <datalist id="assignmentListOptions">
+                        {as.map((homework) => {
+                            return(
+                                <option key={homework._id}>{homework.title}</option>
+                            );
+                        })}
+                    </datalist>
+                </div>
+            </div>
+
+            <button className="wd-rounded-corners-all-around wd-grade-button wd-apply-filters-button"><CiFilter /> Apply Filters</button>
+
+            <div className="table-responsive">
+                <table className="table table-striped table-bordered table-hover" aria-describedby="gradeTable">
+                    <thead className="table-light">
+                        <tr>
+                            <th>Student Name</th>
+                            {as.map((assignment) => (<th className="wd-table-header" key={assignment._id}>{assignment.title}</th>))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {es.map((enrollment) => {
+                            const user = users.find((user) => user._id === enrollment.user);
+                            return (
+                                <tr>
+                                    <td>
+                                        <Link to="/Kanbas/Account" className="wd-student-name">{user?.firstName} {user?.lastName}</Link>
+                                    </td>
+                                    
+                                    {assignments.map((assignment) => {
+                                        const grade = grades.find((grade) => grade.student === enrollment.user && grade.assignment === assignment._id);
+                                        return(
+                                            <td className="wd-table-cell">
+                                                <input className="wd-table-cell" defaultValue={grade?.grade} size={4} type="number" min="1" max="100"/>
+                                            </td>
+                                        );
+                                    })}
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+export default Grades;
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -7,6 +7,7 @@ import Home from "./Home";
 import Breadcrumb from "./Breadcrumb";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
+import Grades from "./Grades";
 
 function Courses() {
     return (
@@ -25,7 +26,7 @@ function Courses() {
                     <Route path="Home" element={<Home/>} />
                     <Route path="Modules" element={<Modules/>} />
                     <Route path="Piazza" element={<h1>Piazza</h1>} />
-                    <Route path="Grades" element={<h1>Grades</h1>} />
+                    <Route path="Grades" element={<Grades />} />
                     <Route path="Assignments" element={<Assignments/>} />
                     <Route path="Assignments/:assignmentId" element={<AssignmentEditor/>}/>
                     <Route path="Quizzes" element={<h1>Quizzes</h1>} />
