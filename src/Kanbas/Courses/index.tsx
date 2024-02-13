ec0b1e821e721f68d6cdf19d5226ab441fdf8440
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-02-13 13:09:17 -0500

    Updated location of Breadcrumb.

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -11,39 +11,40 @@ import Grades from "./Grades";
 
 function Courses() {
     return (
-        <div className="row">
+        <>
             <Breadcrumb/>
+            <div className="row">
+                <CourseNavigation />
 
-            <CourseNavigation />
+                <div className="d-block d-md-none wd-0-lr-padding sticky-top">
+                    <BlackHeader/>
+                </div>
 
-            <div className="d-block d-md-none wd-0-lr-padding sticky-top">
-                <BlackHeader/>
+                <div className="col" style={{paddingLeft: "12vw", paddingRight: "12vw"}}>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="Home" />} />
+                        <Route path="Home" element={<Home/>} />
+                        <Route path="Modules" element={<Modules/>} />
+                        <Route path="Piazza" element={<h1>Piazza</h1>} />
+                        <Route path="Grades" element={<Grades />} />
+                        <Route path="Assignments" element={<Assignments/>} />
+                        <Route path="Assignments/:assignmentId" element={<AssignmentEditor/>}/>
+                        <Route path="Quizzes" element={<h1>Quizzes</h1>} />
+                        <Route path="People" element={<h1>People</h1>} />
+                        <Route path="Panopto Video" element={<h1>Panopto Video</h1>} />
+                        <Route path="Discussions" element={<h1>Discussions</h1>} />
+                        <Route path="Announcements" element={<h1>Announcements</h1>} />
+                        <Route path="Pages" element={<h1>Pages</h1>} />
+                        <Route path="Files" element={<h1>Files</h1>} />
+                        <Route path="Rubrics" element={<h1>Rubrics</h1>} />
+                        <Route path="Outcomes" element={<h1>Outcomes</h1>} />
+                        <Route path="Collaborations" element={<h1>Collaborations</h1>} />
+                        <Route path="Syllabus" element={<h1>Syllabus</h1>} />
+                        <Route path="Settings" element={<h1>Settings</h1>} />
+                    </Routes>
+                </div>
             </div>
-
-            <div className="col" style={{paddingLeft: "12vw", paddingRight: "12vw"}}>
-                <Routes>
-                    <Route path="/" element={<Navigate to="Home" />} />
-                    <Route path="Home" element={<Home/>} />
-                    <Route path="Modules" element={<Modules/>} />
-                    <Route path="Piazza" element={<h1>Piazza</h1>} />
-                    <Route path="Grades" element={<Grades />} />
-                    <Route path="Assignments" element={<Assignments/>} />
-                    <Route path="Assignments/:assignmentId" element={<AssignmentEditor/>}/>
-                    <Route path="Quizzes" element={<h1>Quizzes</h1>} />
-                    <Route path="People" element={<h1>People</h1>} />
-                    <Route path="Panopto Video" element={<h1>Panopto Video</h1>} />
-                    <Route path="Discussions" element={<h1>Discussions</h1>} />
-                    <Route path="Announcements" element={<h1>Announcements</h1>} />
-                    <Route path="Pages" element={<h1>Pages</h1>} />
-                    <Route path="Files" element={<h1>Files</h1>} />
-                    <Route path="Rubrics" element={<h1>Rubrics</h1>} />
-                    <Route path="Outcomes" element={<h1>Outcomes</h1>} />
-                    <Route path="Collaborations" element={<h1>Collaborations</h1>} />
-                    <Route path="Syllabus" element={<h1>Syllabus</h1>} />
-                    <Route path="Settings" element={<h1>Settings</h1>} />
-                </Routes>
-            </div>
-        </div>
+        </>
   );
 }
 export default Courses;
