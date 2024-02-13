74de4a8103937f3050352a929dcacc84dfad2a66
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-02-13 17:45:25 -0500

    Updated relationship between css and tsx Dashboard.

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -24,9 +24,9 @@ function Dashboard() {
             <div className="row wd-dashboard-component">
                 <div className="row row-cols-1 row-cols-md-5 g-4">
                     {courses.map((course) => (
-                        <div key={course._id} className="col" style={{ width: 300 }}>
+                        <div key={course._id} className="col wd-course-card-col" style={{ width: 300 }}>
                             <div className="card">
-                                <img src={`/images/${course.image}`} className="card-img-top" style={{ height: 150 }} alt="blue react card"/>
+                                <img src={`/images/${course.image}`} className="card-img-top wd-card-image" style={{ height: 150 }} alt="blue react card"/>
                                 <div className="card-body">
                                     <Link className="card-title wd-card-title" to={`/Kanbas/Courses/${course._id}/Home`}
                                         style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}><p className="wd-nowrap">{course.name}</p></Link>
