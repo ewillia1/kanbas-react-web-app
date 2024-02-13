d8aa9674207742e41b3b4a2f20008a75191deabe
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-02-13 15:51:17 -0500

    Updated Dashboard.

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -8,31 +8,27 @@ import "./index.css";
 function Dashboard() {
     return (
         <>
-            <div className="d-none d-sm-block sticky-top wd-dashboard" style={{paddingTop: "10px", paddingLeft: "20px", paddingRight: "20px"}}> 
+            <div className="d-none d-sm-block sticky-top wd-dashboard wd-dashboard-component"> 
                 <h1>Dashboard</h1> 
                 <hr/>
             </div>
 
-            <div className="d-block d-sm-none wd-0-lr-padding sticky-top">
-                <BlackHeader/>
-            </div>
-
-            <div className="d-block d-sm-none" style={{paddingTop: "10px", paddingLeft: "20px", paddingRight: "20px"}}>
+            <div className="d-block d-sm-none wd-dashboard-component">
                 <p className="wd-three-dot-menu"><FaEllipsisV /></p>
                 <hr />
             </div>
             
-            <div style={{paddingTop: "10px", paddingLeft: "20px", paddingRight: "20px"}}>
+            <div className="wd-dashboard-component">
                 <h2>Published Courses ({courses.length})</h2> 
                 <hr />
             </div>
 
-            <div className="row" style={{paddingTop: "10px", paddingLeft: "20px", paddingRight: "20px"}}>
+            <div className="row wd-dashboard-component">
                 <div className="row row-cols-1 row-cols-md-5 g-4">
                     {courses.map((course) => (
                         <div key={course._id} className="col" style={{ width: 300 }}>
                             <div className="card">
-                                <img src={`/images/${course.image}`} className="card-img-top" style={{ height: 150 }} alt="react image"/>
+                                <img src={`/images/${course.image}`} className="card-img-top" style={{ height: 150 }} alt="blue react card"/>
                                 <div className="card-body">
                                     <Link className="card-title wd-card-title" to={`/Kanbas/Courses/${course._id}/Home`}
                                         style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}><p className="wd-nowrap">{course.name}</p></Link>
