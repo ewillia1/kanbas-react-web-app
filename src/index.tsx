3fd2566dfe3c49f9ebde8b47db3d1dd31c8fb540
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-02-13 16:51:59 -0500

    Updated spacing in src/index.tsx.

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';    // Imports from App.tsx. The .tsx extension is implied (it is optional in import statements).
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
