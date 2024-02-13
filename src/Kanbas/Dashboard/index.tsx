eb7d441f636156588aec85c58fdad158a8d57a99
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-02-13 13:15:12 -0500

    Updated Dashboard layout.

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -7,41 +7,44 @@ import "./index.css";
 
 function Dashboard() {
     return (
-        <div className="wd-published-courses">
-            <div className="p-4">
-                <div className="d-none d-sm-block sticky-top wd-dashboard">
-                    <h1>Dashboard</h1> <hr/>
-                </div>
+        <>
+            <div className="d-none d-sm-block sticky-top wd-dashboard" style={{paddingTop: "10px", paddingLeft: "20px", paddingRight: "20px"}}> 
+                <h1>Dashboard</h1> 
+                <hr/>
+            </div>
 
-                <div className="d-block d-md-none wd-0-lr-padding sticky-top">
-                    <BlackHeader/>
-                </div>
+            <div className="d-block d-sm-none wd-0-lr-padding sticky-top">
+                <BlackHeader/>
+            </div>
 
-                <div className="d-block d-sm-none">
-                    <p className="wd-three-dot-menu"><FaEllipsisV /></p>
-                    <hr />
-                </div>
-                <h2>Published Courses (12)</h2> <hr />
+            <div className="d-block d-sm-none" style={{paddingTop: "10px", paddingLeft: "20px", paddingRight: "20px"}}>
+                <p className="wd-three-dot-menu"><FaEllipsisV /></p>
+                <hr />
+            </div>
+            
+            <div style={{paddingTop: "10px", paddingLeft: "20px", paddingRight: "20px"}}>
+                <h2>Published Courses (12)</h2> 
+                <hr />
+            </div>
 
-                <div className="row">
-                    <div className="row row-cols-1 row-cols-md-5 g-4">
-                        {courses.map((course) => (
-                            <div key={course._id} className="col" style={{ width: 300 }}>
-                                <div className="card">
-                                    <img src={`/images/${course.image}`} className="card-img-top" style={{ height: 150 }}/>
-                                    <div className="card-body">
-                                        <Link className="card-title wd-card-title" to={`/Kanbas/Courses/${course._id}/Home`}
-                                            style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}><p className="wd-nowrap">{course.name}</p></Link>
-                                        <p className="card-text wd-nowrap">{course.name}</p>
-                                        <Link to={`/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">Go </Link>
-                                    </div>
+            <div className="row" style={{paddingTop: "10px", paddingLeft: "20px", paddingRight: "20px"}}>
+                <div className="row row-cols-1 row-cols-md-5 g-4">
+                    {courses.map((course) => (
+                        <div key={course._id} className="col" style={{ width: 300 }}>
+                            <div className="card">
+                                <img src={`/images/${course.image}`} className="card-img-top" style={{ height: 150 }}/>
+                                <div className="card-body">
+                                    <Link className="card-title wd-card-title" to={`/Kanbas/Courses/${course._id}/Home`}
+                                        style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}><p className="wd-nowrap">{course.name}</p></Link>
+                                    <p className="card-text wd-nowrap">{course.name}</p>
+                                    <Link to={`/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">Go </Link>
                                 </div>
                             </div>
-                        ))}
-                    </div>
+                        </div>
+                    ))}
                 </div>
             </div>
-        </div>
+        </>
     );    
 }
 export default Dashboard
