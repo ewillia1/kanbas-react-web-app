28ac937895c5e5d93598b4596d8582afa5e26677
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-02-15 15:51:23 -0500

    Updated Assignments Editor. Added jQuery. Updated onChange functions.
    Got it so you can edit the assignment name, assignment description, and dates.

--- src/Kanbas/Courses/Assignments/Editor/index.tsx
+++ src/Kanbas/Courses/Assignments/Editor/index.tsx
@@ -2,20 +2,47 @@ import { useNavigate, useParams, Link } from "react-router-dom";
 import { assignments } from "../../../Database";
 import { FaCheckCircle, FaEllipsisV } from "react-icons/fa";
 import "./index.css";
+import { useState } from "react";
 
 function AssignmentEditor() {
     const { assignmentId } = useParams();
     const assignment = assignments.find((assignment) => assignment._id === assignmentId);
     const { courseId } = useParams();
     const navigate = useNavigate();
-    const handleSave = () => {
-        console.log("Actually saving assignment TBD in later assignments");
-        navigate(`/Kanbas/Courses/${courseId}/Assignments`);
-    };
+    const [assignmentTitle, setAssignmentTitle] = useState(assignment?.title);
+    const [assignmentDescription, setAssignmentDescription] = useState(assignment?.subtitle);
+    const [assignmentPoints, setAssignmentPoints] = useState(assignment?.points);
+    const [assignmentDueDate, setAssignmentDueDate] = useState('2021-01-01');
+    const [assignmentAvailableFromDate, setAssignmentAvailableFromDate] = useState('2021-01-01');
+    const [assignmentUntilDate, setAssignmentUntilDate] = useState('2021-01-01');
 
-    function myChangeFunction() {
-      // TODO document why this function 'myChangeFunction' is empty
+    let clickedAtLeastOneCheckbox = false;
+
+    function handleSave() {
+        console.log("in handleSave");
+        if (clickedAtLeastOneCheckbox) {
+            console.log("Actually saving assignment TBD in later assignments");
+            navigate(`/Kanbas/Courses/${courseId}/Assignments`);
+        } else {
+            alert("Please check at least one Online Entry Options.");
+        }
     }
+
+    // Check to make sure that at least one Online Entry Options checkbox is checked.
+    $(function(){
+        var requiredCheckboxes = $('input:checkbox[required]');
+        requiredCheckboxes.on('change',function(){
+            if($('input:checkbox[type=checkbox]').is(":checked") == true) {
+                requiredCheckboxes.removeAttr('required');
+                console.log("if");
+                clickedAtLeastOneCheckbox = true;
+            } else {
+                requiredCheckboxes.attr('required', 'required');
+                console.log("else");
+                clickedAtLeastOneCheckbox = false;
+            }
+        });
+    });
 
     return (
         <div>
@@ -27,17 +54,17 @@ function AssignmentEditor() {
             <form>
                 <div className="mb-3">
                     <label htmlFor="assignmentName" className="form-label">Assignment Name</label>
-                    <input type="text" className="form-control" id="assignmentName" value={assignment?.title} onChange={myChangeFunction}/>
+                    <input type="text" className="form-control" id="assignmentName" value={assignmentTitle} onChange={e => setAssignmentTitle(e.target.value)}/>
                 </div>
                 <div className="mb-3">
-                    <textarea className="form-control" id="description" rows={3} value={assignment?.subtitle} onChange={myChangeFunction}></textarea>
+                    <textarea className="form-control" id="description" rows={3} value={assignmentDescription} onChange={e => setAssignmentDescription(e.target.value)}></textarea>
                 </div>
 
                 <div className="wd-bottom-section">
                     <div className="row mb-3">
                         <label htmlFor="points" className="col-sm-4 col-form-label wd-assign-edit-label">Points</label>
                         <div className="col-sm-8">
-                            <input type="number" min="0" className="form-control" id="points" value={assignment?.points} onChange={myChangeFunction}/>
+                            <input type="number" min="0" className="form-control" id="points" value={assignmentPoints} onChange={e => setAssignmentPoints(e.target.value)}/>
                         </div>
                     </div>
                     <div className="row mb-3">
@@ -85,19 +112,19 @@ function AssignmentEditor() {
                     <div className="row mb-3">
                         <p className="col-sm-4 col-form-label wd-assign-edit-label">Online Entry Options</p>
                         <div className="col-sm-8" id="onlineEntryOptions">
-                            <input className="form-check-input" type="checkbox" name="check-online-entry-options" id="chkbox-textEntry"/>
+                            <input className="form-check-input" type="checkbox" name="check-online-entry-options" id="chkbox-textEntry" required/>
                             <label htmlFor="chkbox-textEntry" style={{paddingLeft: "5px"}}>Text Entry</label><br/>
                 
-                            <input className="form-check-input" type="checkbox" name="check-online-entry-options" id="chkbox-websiteURL"/>
+                            <input className="form-check-input" type="checkbox" name="check-online-entry-options" id="chkbox-websiteURL" required/>
                             <label htmlFor="chkbox-websiteURL" style={{paddingLeft: "5px"}}>Website URL</label><br/>
                 
-                            <input className="form-check-input" type="checkbox" name="check-online-entry-options" id="chkbox-mediaRec"/>
+                            <input className="form-check-input" type="checkbox" name="check-online-entry-options" id="chkbox-mediaRec" required/>
                             <label htmlFor="chkbox-mediaRec" style={{paddingLeft: "5px"}}>Media Recordings</label><br/>
                 
-                            <input className="form-check-input" type="checkbox" name="check-online-entry-options" id="chkbox-studentAnno"/>
+                            <input className="form-check-input" type="checkbox" name="check-online-entry-options" id="chkbox-studentAnno" required/>
                             <label htmlFor="chkbox-studentAnno" style={{paddingLeft: "5px"}}>Student Annotation</label><br/>
                 
-                            <input className="form-check-input" type="checkbox" name="check-online-entry-options" id="chkbox-fileUploads"/>
+                            <input className="form-check-input" type="checkbox" name="check-online-entry-options" id="chkbox-fileUploads" required/>
                             <label htmlFor="chkbox-fileUploads" style={{paddingLeft: "5px"}}>File Uploads</label><br/><br/>
                         </div>
                     </div>
@@ -112,16 +139,16 @@ function AssignmentEditor() {
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="dueDate" className="form-label">Due</label>
-                                <input id="dueDate" className="form-control" type="date" value="2021-01-01" onChange={myChangeFunction}/>
+                                <input id="dueDate" className="form-control" type="date" value={assignmentDueDate} onChange={e => setAssignmentDueDate(e.target.value)}/>
                             </div>
                             <div className="row g-2">
                                 <div className="col-sm">
                                     <label htmlFor="availableFrom" className="form-label">Available from</label>
-                                    <input id="availableFrom" className="form-control" type="date" value="2021-01-01" onChange={myChangeFunction}/>
+                                    <input id="availableFrom" className="form-control" type="date" value={assignmentAvailableFromDate} onChange={e => setAssignmentAvailableFromDate(e.target.value)}/>
                                 </div>
                                 <div className="col-sm">
                                     <label htmlFor="untilDate" className="form-label">Until</label>
-                                    <input id="untilDate" className="form-control" type="date" value="2021-01-01" onChange={myChangeFunction}/>
+                                    <input id="untilDate" className="form-control" type="date" value={assignmentUntilDate} onChange={e => setAssignmentUntilDate(e.target.value)}/>
                                 </div>
                             </div>
                             <div className="mb-3">
