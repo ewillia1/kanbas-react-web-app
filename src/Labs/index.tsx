6d5b3cb9d63131ee4b814f19c360750c29100eea
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-02-19 15:04:02 -0500

    Finished 2.4.2 Create a Hello World Redux component.

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,3 +1,5 @@
+import { Provider } from "react-redux";
+import store from "./store";
 import Nav from "../Nav";
 import Assignment3 from "./a3";
 import Assignment4 from "./a4";
@@ -5,14 +7,16 @@ import {Routes, Route, Navigate} from "react-router";
 
 function Labs() {
 return (
-    <div>
-        <Nav/>
-        <Routes>
-            <Route path="/" element={<Navigate to="/Labs/a3"/>}/>
-            <Route path="a3/*" element={<Assignment3/>}/>
-            <Route path="a4" element={<Assignment4/>}/>
-        </Routes>
-    </div>
+    <Provider store={store}>
+        <div className="container-fluid">
+            <Nav/>
+            <Routes>
+                <Route path="/" element={<Navigate to="/Labs/a3"/>}/>
+                <Route path="a3/*" element={<Assignment3/>}/>
+                <Route path="a4" element={<Assignment4/>}/>
+            </Routes>
+        </div>
+    </Provider>
 );
 }
 export default Labs;
