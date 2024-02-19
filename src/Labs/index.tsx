2a1875f53d7754e15a86d17a754e923ac7f6a4c8
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-02-19 13:42:49 -0500

    Updated Labs/index.tsx to remove an error.
    Error that was fixed:
    You rendered descendant <Routes> (or called `useRoutes()`) at "/Labs/a3" (under <Route path="a3">) but the parent route path has no trailing "*". This means if you navigate deeper, the parent won't match anymore and therefore the child routes will never render.
    
    Please change the parent <Route path="a3"> to <Route path="a3/*">.

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,16 +3,16 @@ import Assignment3 from "./a3";
 import Assignment4 from "./a4";
 import {Routes, Route, Navigate} from "react-router";
 
-  function Labs() {
-    return (
-        <div>
-            <Nav/>
-            <Routes>
-                <Route path="/" element={<Navigate to="a3"/>}/>
-                <Route path="a3" element={<Assignment3/>}/>
-                <Route path="a4" element={<Assignment4/>}/>
-            </Routes>
-        </div>
-    );
+function Labs() {
+return (
+    <div>
+        <Nav/>
+        <Routes>
+            <Route path="/" element={<Navigate to="/Labs/a3"/>}/>
+            <Route path="a3/*" element={<Assignment3/>}/>
+            <Route path="a4" element={<Assignment4/>}/>
+        </Routes>
+    </div>
+);
 }
 export default Labs;
