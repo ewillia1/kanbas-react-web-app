de6179b4a81c5e21314370de590a659a86e73a87
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-02-22 09:12:07 -0500

    Updated spacing in ClickEvent.tsx.

--- src/Labs/a4/ClickEvent.tsx
+++ src/Labs/a4/ClickEvent.tsx
@@ -6,11 +6,11 @@ function ClickEvent() {
     const lifeIs = (good: string) => {
         alert(`Life is ${good}`);
     };
-
+    
     return (
         <div>
             <h3>Click Event</h3>
-            <button className="btn btn-primary me-4 mb-4" onClick={hello}>Click Hello</button>
+            <button className="btn btn-primary me-2 mb-4" onClick={hello}>Click Hello</button>
             <button className="btn btn-primary me-2 mb-4" onClick={() => lifeIs("Good!")}>Click Good</button>
             <button className="btn btn-primary mb-4" onClick={() => { hello(); lifeIs("Great!");}}>Click Hello 3</button>
         </div>
