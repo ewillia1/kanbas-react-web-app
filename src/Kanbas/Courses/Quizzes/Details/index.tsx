16dec3350cf887d58bd99971d7b9f8e362c2322f
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-03-04 17:52:07 -0500

    Working on Quizzes/Details.

--- src/Kanbas/Courses/Quizzes/Details/index.tsx
+++ src/Kanbas/Courses/Quizzes/Details/index.tsx
@@ -1,7 +1,128 @@
+import { FaEllipsisV } from "react-icons/fa";
+import { PiPencilLight } from "react-icons/pi";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router";
+import { KanbasState } from "../../../store";
+import { FiSlash } from "react-icons/fi";
+import { useEffect, useState } from "react";
+import { selectQuiz } from "../quizzesReducer";        // Import reducer functions to add, delete, and update quizzes.
+
 function QuizDetails() {
+    const { courseId } = useParams();
+    console.log("courseId = " + courseId);
+    const { quizId } = useParams();
+    console.log("quizId = " + quizId);
+    const quizList = useSelector((state: KanbasState) => state.quizzesReducer.quizzes);  // Retrieve current state variables modules and module from reducer.
+    const quiz = useSelector((state: KanbasState) => state.quizzesReducer.quiz);
+    console.log("quiz = " + JSON.stringify(quiz));
+
+    const dispatch = useDispatch();             // Get dispatch to call reducer functions.
+    const navigate = useNavigate();
+
+    function handleEditQuiz() {
+        console.log("In handleEditQuiz");
+        navigate(`/Kanbas/Courses/${courseId}/Quizzes/DetailsEditor/DetailsEditor`);
+    }
+
+    // If user is coming from clicking add quiz, set values to default values.
+    // Else the user is coming from clicking an old quiz, so set values to the values of the quiz clicked.
+    // Only run the effect on the initial render.
+    useEffect(() => {
+        // Runs only on the first render.
+        if ( quizId !== undefined ) {
+            if (quizId.localeCompare("DetailsEditor")) {
+                const a = quizList.find((quiz) => quiz._id === quizId);
+                dispatch(selectQuiz(a));
+            }
+        }
+    }, []);
+    
     return(
         <>
-            <h1>Quiz Details</h1>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+
+                {/* TODO: Clicking Publish button changes Unpublished to Published, etc. */}
+                <button type="button" className="btn btn-light btn-outline-dark mt-1"><FiSlash />Unpublished</button>
+                {/* TODO: Click Preview button to navigate to Quiz Preview. */}
+                <button type="button" className="btn btn-light btn-outline-dark mt-1">Preview</button>
+                <button type="button" className="btn btn-light btn-outline-dark mt-1" onClick={handleEditQuiz}><PiPencilLight style = {{transform: 'rotate(270deg)' }} />Edit</button>
+                <button type="button" className="btn btn-light btn-outline-dark mt-1"><FaEllipsisV /></button>
+            </div>
+            <hr/>
+
+            <h1>{quiz?.title}</h1>
+
+            <div className="row mb-3">
+                <label className="col" style={{textAlign: "right"}}><b>Quiz Type</b></label>
+                <label className="col">Graded Quiz</label>
+            </div>
+            <div className="row mb-3">
+                <label className="col" style={{textAlign: "right"}}><b>Points</b></label>
+                <label className="col">{quiz.points}</label>
+            </div>
+            <div className="row mb-3">
+                <label className="col" style={{textAlign: "right"}}><b>Assignment Group</b></label>
+                <label className="col">QUIZZES</label>
+            </div>
+            <div className="row mb-3">
+                <label className="col" style={{textAlign: "right"}}><b>Shuffle Answers</b></label>
+                <label className="col">No</label>
+            </div>
+            <div className="row mb-3">
+                <label className="col" style={{textAlign: "right"}}><b>Time Limit</b></label>
+                <label className="col">30 Minutes</label>
+            </div>
+            <div className="row mb-3">
+                <label className="col" style={{textAlign: "right"}}><b>Multiple Attempts</b></label>
+                <label className="col">No</label>
+            </div>
+            <div className="row mb-3">
+                <label className="col" style={{textAlign: "right"}}><b>View Responses</b></label>
+                <label className="col">Always</label>
+            </div>
+            <div className="row mb-3">
+                <label className="col" style={{textAlign: "right"}}><b>Show Correct Answers</b></label>
+                <label className="col">Immediately</label>
+            </div>
+            <div className="row mb-3">
+                <label className="col" style={{textAlign: "right"}}><b>One Question at a Time</b></label>
+                <label className="col">Yes</label>
+            </div>
+            <div className="row mb-3">
+                <label className="col" style={{textAlign: "right"}}><b>Require Respondus LockDown Browser</b></label>
+                <label className="col">No</label>
+            </div>
+            <div className="row mb-3">
+                <label className="col" style={{textAlign: "right"}}><b>Required to View Quiz Results</b></label>
+                <label className="col">No</label>
+            </div>
+            <div className="row mb-3">
+                <label className="col" style={{textAlign: "right"}}><b>Webcam Required</b></label>
+                <label className="col">No</label>
+            </div>
+            <div className="row mb-3">
+                <label className="col" style={{textAlign: "right"}}><b>Lock Questions After Answering</b></label>
+                <label className="col">No</label>
+            </div>
+
+            <table className="table" style={{marginTop: "40px"}}>
+                <thead>
+                    <tr>
+                    <th scope="col">Due</th>
+                    <th scope="col">For</th>
+                    <th scope="col">Available from</th>
+                    <th scope="col">Until</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>{quiz.dueDate}</td>
+                    <td>{quiz.forAccess}</td>
+                    <td>{quiz.availableFromDate}</td>
+                    <td>{quiz.untilDate}</td>
+                    </tr>
+                </tbody>
+            </table>
         </>
     );
 }
