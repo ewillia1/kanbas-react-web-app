bd5a44e8d6a7e4c47603d250cd01c16797119a0e
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-03-11 12:01:36 -0400

    Created an Assignment5 React.js component to test the Node HTTP server.

--- src/Labs/a5/index.tsx
+++ src/Labs/a5/index.tsx
@@ -1,7 +1,14 @@
+import "./index.css";
+
 function Assignment5() {
     return (
-        <div className="container">
+        <div>
             <h2>Assignment 5</h2>
+            <div className="wd-container wd-border wd-round-xlarge">
+                <a href="http://localhost:4000/a5/welcome" style={{textDecoration: "none", color: "black"}} >
+                    Welcome
+                </a>
+            </div>
         </div>
     );
 }
