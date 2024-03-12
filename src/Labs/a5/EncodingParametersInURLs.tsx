082f9c9bec898ec865980f7418af8f698d8152b8
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-03-12 10:59:09 -0400

    3.4.3 Sending and fetching data to and from an HTTP server.

--- src/Labs/a5/EncodingParametersInURLs.tsx
+++ src/Labs/a5/EncodingParametersInURLs.tsx
@@ -20,22 +20,39 @@ function EncodingParametersInURLs() {
         fetchWelcome();
     }, []);
 
+    const [result, setResult] = useState(0);
+    const fetchSum = async (a: any, b: any) => {
+      const response = await axios.get(`http://localhost:4000/a5/add/${a}/${b}`);
+      setResult(response.data);
+    };  
+
+    const fetchSubtraction = async (a: any, b: any) => {
+        const response = await axios.get(`http://localhost:4000/a5/subtract/${a}/${b}`);
+        setResult(response.data);
+    };
+
     return (
         <div>
             <h3 className="mt-2">Encoding Parameters In URLs</h3>
             <h4>Integrating React with APIs</h4>
-            <h5>Fetching Welcome</h5>
+            <h5>Fetching Welcome From Server:</h5>
             <h6>{welcome}</h6>
 
             <h4>Calculator</h4>
 
             <div className="mb-3">
                 <input id="valueA" className="form-control mb-2" type="number" value={a} onChange={(e) => setA(parseInt(e.target.value))}/>
-                <input id="valueB" className="form-control" type="number" onChange={(e) => setB(parseInt(e.target.value))} value={b}/>
+                <input id="valueB" className="form-control mb-2" type="number" value={b} onChange={(e) => setB(parseInt(e.target.value))}/>
+                <input id="resultValue" className="form-control mb-2" value={result} type="number" readOnly />
+
+                <h3>Fetch Result</h3>
+                <div className="d-grid gap-2">
+                    <button onClick={() => fetchSum(a, b)} className="btn btn-primary me-2">Fetch Sum of {a} + {b}</button>
+                    <button onClick={() => fetchSubtraction(a, b)} className="btn btn-danger me-2">Fetch Substraction of {a} - {b}</button>
+                </div>
             </div>
 
             <h3>Path Parameters</h3>
-
             <a href={`http://localhost:4000/a5/add/${a}/${b}`} type="button" className="btn btn-primary me-2 mb-2">
                 Add {a} + {b}
             </a>
@@ -50,7 +67,6 @@ function EncodingParametersInURLs() {
             </a>
 
             <h3>Query Parameters</h3>
-
             <a className="btn btn-primary me-2 mb-2" href={`http://localhost:4000/a5/calculator?operation=add&a=${a}&b=${b}`}>
                 Add {a} + {b}
             </a>
