b7c019e691c10cdcee2f7cb9f55cc4c548393bf6
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-03-11 20:08:54 -0400

    Added class container to div in a5/index.tsx.

--- src/Labs/a5/index.tsx
+++ src/Labs/a5/index.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 
 function Assignment5() {
     return (
-        <div>
+        <div className="container">
             <h2>Assignment 5</h2>
             <div className="wd-container wd-border wd-round-xlarge">
                 <a href="http://localhost:4000/a5/welcome" style={{textDecoration: "none", color: "black"}} >
