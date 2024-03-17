7ceeb0e8a171dfea7107536cb543e04e2dff2401
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-03-17 11:47:04 -0400

    Updated dateUtil.js. Removed unused code.

--- src/Kanbas/Util/dateUtil.js
+++ src/Kanbas/Util/dateUtil.js
@@ -1,21 +1,9 @@
-export function getCurrentDate(separator = '-') {
-    let newDate = new Date()
-    let date = newDate.getDate();
-    let month = newDate.getMonth() + 1;
-    let year = newDate.getFullYear();
-    return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`
-}
-
 export function availableText(quiz) {
-    console.log("quiz = " + JSON.stringify(quiz));
     let today = new Date();
-    console.log("today = " + today);
     let partsA = quiz.availableFromDate.split('-');
     let availableDate = new Date(partsA[0], partsA[1] - 1, partsA[2]);
-    console.log("availableDate = " + availableDate);
     let partsB = quiz.untilDate.split('-');
     let availableUntilDate = new Date(partsB[0], partsB[1] - 1, partsB[2]);
-    console.log("availableUntilDate = " + availableUntilDate);
 
     if (availableUntilDate < today) {       // If Available Until Date is before today. CLOSED.
         return 1;
