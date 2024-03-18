2cc5d038f92408323c288c33a780985c13443ab9
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-03-18 19:21:11 -0400

    Added getDateObject function to Util/dateUtil.js.

--- src/Kanbas/Util/dateUtil.js
+++ src/Kanbas/Util/dateUtil.js
@@ -29,3 +29,11 @@ export function getCurrentTime(date) {
     console.log("timeStatement = " + timeStatement);
     return timeStatement;
 }
+
+export function getDateObject(stringDate) {
+    var parts = stringDate.split('-');
+    // Pay attention to the month (parts[1]); JavaScript counts months from 0:
+    // January - 0, February - 1, etc.
+    var resultDate = new Date(parts[0], parts[1] - 1, parts[2]); 
+    return resultDate;
+}
